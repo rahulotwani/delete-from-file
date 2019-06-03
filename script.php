5b8de25f7fd04d1a48038c386ea47ed42249aed9
Rahul Otwani <rahulotwani7@gmail.com>
2019-06-03 22:43:16 +0530

    Create script.php

---
+++ script.php
@@ -0,0 +1,20 @@
+<?php 
+	
+function deleteLineInFile($file,$string)
+{
+	$i=0;$array=array();
+	
+	$read = fopen($file, "r") or die("can't open the file");
+	while(!feof($read)) {
+		$array[$i] = fgets($read);	
+		++$i;
+	}
+	fclose($read);
+	
+	$write = fopen($file, "w") or die("can't open the file");
+	foreach($array as $a) {
+		if(!strstr($a,$string)) fwrite($write,$a);
+	}
+	fclose($write);
+}
+?>
